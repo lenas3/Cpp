7053b7055396db91761432d4c96e0445dabb36c9
Aleyna Say <asay@k1m01s06.42istanbul.com.tr>
2026-09-07 21:11:48 +0300

    feat(cpp00/ex02): add getNbAccounts and a few explanations

--- C00/ex02/Account.cpp
+++ C00/ex02/Account.cpp
@@ -8,13 +8,18 @@ Account::Account( int initial_deposit ):
     _nbWithdrawals(0)
 {
     //degiskenleri burda güncellemem lazim
+    Account::_nbAccounts++; // her constructor da bir hesap oluşturğu için bir tane artırılacak şekilde yaptık.
+    //çıktıyı, değişkeni güncelledikten sonra aşağıdaki gibi yazdırıcam.
     _displayTimestamp(); // bbu fonksiyonu öyle yazmam lazim ki [340957306735] gibi bir şey yazsin.
     std::cout <<"index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
 }
 
-int	Account::getNbAccounts( void )
+int	Account::getNbAccounts( void ) // _nbAccounts, o ana kadar oluşturulmuş olan toplam account nesnesisayısını tutuyor.
 {
-
+    // _NbAccounts ststaişc bir üye olduğu için constructor her çalıştığında 1 artacak.
+    // yani tüm objeler aynı tek bir nbAccount değişkenini paylaşıyor. Bu değişken sınıfa ait, OBJEYE DEĞİL.
+    // her yeni nesne oluştuğunda constructor ile nbAccounts++ olacağı için burada sadece değişkeni döndürüyoruz.  
+    return Account::_nbAccounts; 
 }
 int	Account::getTotalAmount( void )
 {

693454cc0d8bd592226fcb04e2ecf2d85d6bdf2c
Aleyna Say <asay@k1m07s08.42istanbul.com.tr>
2026-09-16 23:08:52 +0300

    feat(ex01): add class declarations

--- C00/ex01/PhoneBook.hpp
+++ C00/ex01/PhoneBook.hpp
@@ -2,16 +2,27 @@
 #define PHONEBOOK_H
 
 #include <string>
+#include <iostream>
+#include <iomanip>
 #include "Contact.hpp"
 
 class PhoneBook
 {
     public:
-        int getNbContacts;
+        int getNbContacts(void) const;  //bunu public yaptım çünkü dönen değerine mainden erişmem lazim
+        void displayAdd(void);
+        void displaySearch(void);
+        void displayExit(void);
+
+        PhoneBook(void);
+        ~PhoneBook(void);
 
     private:
-        Contact contacts[8];
+        Contact _contacts[8];
+        void displayColumns(void);
+        std::string fixLength(std::string word);
         int _nbContacts;
+        int _index;
 };
 
 #endif
